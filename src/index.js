f2709bb9b979f1bb38ea7d1a112eb3b4940cb181
Robert Gacki <r.gacki@gmail.com>
2017-03-16 16:46:53 +0100

    section 2, lecture 22

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCE-cWblxRnEeAUmes4SULdsIPiO_S7auM';
 
+class App extends Component {
+    constructor (props) {
+        super(props);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setStates({videos});
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
