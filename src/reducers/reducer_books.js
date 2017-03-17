302f549bb01d5b2a11a62e9d6c237858880c546d
Robert Gacki <r.gacki@gmail.com>
2017-03-17 14:44:11 +0100

    section 4, lecture 46

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'JS dfhksjdh'},
+        {title: '#2 fdsfsdf'},
+        {title: '#3 dfhksjdh'},
+        {title: '#4 dfhksjdh'}
+    ];
+}
