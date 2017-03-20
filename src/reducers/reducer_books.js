2ba9e02ee47316ccb30dc8652cb25047fd69a235
Robert Gacki <r.gacki@gmail.com>
2017-03-20 13:57:30 +0100

    section 4, lecture 50

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        {title: 'JS dfhksjdh'},
-        {title: '#2 fdsfsdf'},
-        {title: '#3 dfhksjdh'},
-        {title: '#4 dfhksjdh'}
+        {title: 'JS dfhksjdh', pages: 100},
+        {title: '#2 fdsfsdf', pages: 201},
+        {title: '#3 dfhksjdh', pages: 3},
+        {title: '#4 dfhksjdh', pages: 4400}
     ];
 }
